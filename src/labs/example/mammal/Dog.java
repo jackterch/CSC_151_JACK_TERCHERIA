a2915f5b9f0ac5b3bf71c0afdeca74df129e1bfa
SANDHILLS\LH218-07 <jad.talbert@gmail.com>
2025-01-29 11:45:48 -0500

    new version Dog.java

--- src/labs/example/mammal/Dog.java
+++ src/labs/example/mammal/Dog.java
@@ -2,10 +2,6 @@ package src.labs.example.mammal;
 
 public class Dog extends mammal {
 
-    public void Dog() {
-        
-    } 
-
     public void Bark() {
         System.out.println("woof...woof...woof");
     }
