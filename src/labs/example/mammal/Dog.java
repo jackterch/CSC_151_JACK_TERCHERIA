32d13849d8a51bb39629b9c29584cba685ba6156
SANDHILLS\LH218-07 <jad.talbert@gmail.com>
2025-01-29 11:44:47 -0500

    Dog file added

---
+++ src/labs/example/mammal/Dog.java
@@ -0,0 +1,16 @@
+package src.labs.example.mammal;
+
+public class Dog extends mammal {
+
+    public void Dog() {
+        
+    } 
+
+    public void Bark() {
+        System.out.println("woof...woof...woof");
+    }
+
+    public void Fetch() {
+
+    }
+}
